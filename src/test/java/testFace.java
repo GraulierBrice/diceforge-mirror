cd1bc597fa0fe5ec90f58aab819302c12bd9da33
Elzebalth <vialestephane1@gmail.com>
2018-10-12 17:15:16 +0200

    #7 creating FacePdS and FacePdS test

--- src/test/java/testFace.java
+++ src/test/java/testFace.java
@@ -10,8 +10,18 @@ public class testFace {
         FaceGold g1=new FaceGold(1);
         g1.giveReward(player);
         assertTrue(player.getGold()==1);
-        FaceGold g3=new FaceGold(18);
-        g3.giveReward(player);
+        FaceGold g18=new FaceGold(18);
+        g18.giveReward(player);
         assertTrue(player.getGold()==12);
+    }
+
+    @Test public void facePdS(){
+        Player player=new Player();
+        FacePdS PdS1=new FacePdS(1);
+        PdS1.giveReward(player);
+        assertTrue(player.getPdS()==1);
+        FacePdS PdS8=new FacePdS(8);
+        PdS8.giveReward(player);
+        assertTrue(player.getPdS()==6);
     }
 }
