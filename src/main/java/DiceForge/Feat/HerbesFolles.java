ce9ec10154da15762b5d392545559b04218e265c
AnEdge <vincent.coppe@hotmail.com>
2018-10-25 10:01:00 +0200

    #16 Adding the feat "HerbesFolles"
    adding the feat herbes folles that gives the player 3 moon point and 3 gold for 1 sun point it also give 2 honour to the player at the end of the game

---
+++ src/main/java/DiceForge/Feat/HerbesFolles.java
@@ -0,0 +1,20 @@
+package DiceForge.Feat;
+
+import DiceForge.Player;
+
+public class HerbesFolles extends Feat {
+
+    public HerbesFolles() {
+        super(2,0,1);
+    }
+
+    public void setPlayer(Player player) {
+        super.setPlayer(player);
+        this.effect();
+    }
+
+    public void effect(Object... o) {
+        owner.addGold(3);
+        owner.addPdL(3);
+    }
+}
