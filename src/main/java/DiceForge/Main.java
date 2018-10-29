d4a245d94d7da124e13d7dc7afbdc13b0a8ed81c
Elzebalth <vialestephane1@gmail.com>
2018-10-29 03:42:21 +0100

    #36 made modification so you can run the game with two differents mods
    there is two mod the first one is the classic one where you follow the whole game and the second one is a stats mod when you launch tons of games

--- src/main/java/DiceForge/Main.java
+++ src/main/java/DiceForge/Main.java
@@ -7,21 +7,12 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        Referee R = new Referee(new RandomAI(),new RandomAI(),new LunarAI());
-        Forge forge=new Forge(R);
-        World world=new World(R);
+        Referee R = new Referee(new RandomAI(), new RandomAI(), new LunarAI(),new LunarAI());
+        Forge forge = new Forge(R);
+        World world = new World(R);
         R.addForge(forge);
         R.addWorld(world);
-        while(R.getRound()<=R.getMaxRound()) {
-            System.out.println("Nous sommes au tour : " + R.getRound() + "\n");
-            for (int i = 0; i < R.getNumberPlayer(); i++) {
-                R.faveur();
-                R.choixAction(R.getPlayer(R.getTurnPlayer()).chooseAction());
-                R.printLog();
-                R.nextPlayer();
-            }
-        }
-    	R.printWinner();
+        R.game(30000);
     }
 }
 
--- src/main/java/DiceForge/Referee.java
+++ src/main/java/DiceForge/Referee.java
@@ -4,77 +4,108 @@ import DiceForge.Feat.*;
 import java.util.ArrayList;
 
 public class Referee {
-    private ArrayList<Player> players=new ArrayList<>();
+    private ArrayList<Player> players = new ArrayList<>();
     private static Forge forge;
     private static World world;
-    private Island island;
-    private int turnPlayer,round,maxRound;
+    private int turnPlayer, round, maxRound;
+    public ArrayList<Integer> nbVictoire = new ArrayList<>();
 
-    public Referee(Player... players){
-        ArrayList<Feat> island1 = new ArrayList<>();
-        for(int i=0;i<players.length;i++) {
+    public Referee(Player... players) {
+        for (int i = 0; i < players.length; i++) {
             this.players.add(players[i]);
-            island1.add(new Hammer());
-            island1.add(new Chest());
+            nbVictoire.add(0);
         }
-        this.island=new Island(island1);
-        this.turnPlayer=0;
-        this.round=1;
-        this.maxRound = (this.getNumberPlayer()==3) ? 10 : 9;
+        this.turnPlayer = 0;
+        this.round = 1;
+        this.maxRound = (this.getNumberPlayer() == 3) ? 10 : 9;
+
 
     }
 
-    public void addForge(Forge forge){this.forge=forge;}
-    public void addWorld(World world){this.world=world;}
+    public void addForge(Forge forge) {
+        this.forge = forge;
+    }
+
+    public void addWorld(World world) {
+        this.world = world;
+    }
 
     /* Accessor */
-    public int getTurnPlayer(){return this.turnPlayer;}
-    public int getNumberPlayer(){return this.players.size();}
-    public Player getPlayer(int n){return players.get(n);}
-    public int getRound(){return this.round;}
-    public int getMaxRound(){return this.maxRound;}
-    public static Forge getForge(){return forge;}
-    public static World getWorld(){return world;}
-        /* Mutator */
-    public void setPlayers(ArrayList<Player> players){this.players = players;}
-    public void nextPlayer(){
+    public int getTurnPlayer() {
+        return this.turnPlayer;
+    }
+
+    public int getNumberPlayer() {
+        return this.players.size();
+    }
+
+    public Player getPlayer(int n) {
+        return players.get(n);
+    }
+
+    public int getRound() {
+        return this.round;
+    }
+
+    public int getMaxRound() {
+        return this.maxRound;
+    }
+
+    public static Forge getForge() {
+        return forge;
+    }
+
+    public static World getWorld() {
+        return world;
+    }
+
+    /* Mutator */
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public void nextPlayer() {
         this.turnPlayer++;
-        if(this.turnPlayer == this.getNumberPlayer()) {
+        if (this.turnPlayer == this.getNumberPlayer()) {
             turnPlayer = 0;
             round++;
         }
     }
 
-    public void choixAction(String action){
+    public void choixAction(String action, int number) {
 
         Player turnP = this.getPlayer(this.turnPlayer);
 
         switch (action) {
-            case "passe" :
-                System.out.println("\033[34mJoueur "+(this.turnPlayer+1)+" passe son tour\u001B[0m");
-                break;   
-            case "forge" :
-                System.out.println("\033[34mJoueur "+(this.turnPlayer+1)+" peut acheter une face\u001B[0m");
-                Pool pool=forge.getPool(turnP.choosePool());
-                System.out.println(pool.isEmpty());
-                if(!pool.isEmpty() && turnP.getGold()>=pool.getPrice()) {
+            case "passe":
+                if (number == 1)
+                    System.out.println("\033[34mJoueur " + (this.turnPlayer + 1) + " passe son tour\u001B[0m");
+                break;
+            case "forge":
+                if (number == 1)
+                    System.out.println("\033[34mJoueur " + (this.turnPlayer + 1) + " peut acheter une face\u001B[0m");
+                Pool pool = forge.getPool(turnP.choosePool());
+                if (!pool.isEmpty() && turnP.getGold() >= pool.getPrice()) {
                     int poolFace = turnP.choosePoolFace(pool);
                     int dice = turnP.chooseDice();
                     int diceFace = turnP.chooseDiceFace(turnP.chooseDice());
                     this.buy(pool, poolFace, dice, diceFace);
                 }
-                break;   
-            case "exploit" :
-                System.out.println("\033[34mJoueur "+(this.turnPlayer+1)+" peut choisir un exploit à réaliser\u001B[0m");
+                break;
+            case "exploit":
+                if (number == 1)
+                    System.out.println("\033[34mJoueur " + (this.turnPlayer + 1) + " peut choisir un exploit à réaliser\u001B[0m");
                 turnP.chooseIsland();
+                Island island = this.world.getIsland(turnP.getCurrentIsland());
                 Class exploit = turnP.chooseFeat();//l'exploit sur l'ile qu'il va choisir
-                if(this.island.isIn(exploit) && (turnP.getPdL()>=this.island.getFeat(exploit).getPricePdL() || turnP.getPdS()>=this.island.getFeat(exploit).getPricePdS())){
+                if (island.isIn(exploit) && (turnP.getPdL() >= island.getFeat(exploit).getPricePdL() || turnP.getPdS() >= island.getFeat(exploit).getPricePdS())) {
                     turnP.removePdL(1);
-                    this.island.getFeat(exploit).setPlayer(turnP);
-                    System.out.println("\033[34mJoueur "+(this.turnPlayer+1)+" réalise l'exploit "+exploit.getName().split("\\.")[2]+"\u001B[0m");
-                    this.island.removeFeat(exploit);
+                    island.getFeat(exploit).setPlayer(turnP);
+                    if (number == 1)
+                        System.out.println("\033[34mJoueur " + (this.turnPlayer + 1) + " réalise l'exploit " + exploit.getName().split("\\.")[2] + "\u001B[0m");
+                    island.removeFeat(exploit);
                 }
-                break;      
+                break;
         }
     }
 
@@ -90,26 +121,28 @@ public class Referee {
         -Indicates player actions in blue
         -Indicates dice faces rolled in yellow    
     */
-    public void printLog(){
-        for (Player p : players) {
-            System.out.println(this.players.indexOf(p) == this.getTurnPlayer() ? "\u001B[31m" + "information joueur: " + (this.players.indexOf(p) + 1) + "\u001B[0m" : "information joueur: " + (this.players.indexOf(p) + 1));
-            System.out.println("Honour: " + p.getHonour()+"\nGold: " + p.getGold() + "/" + p.getMaxGold()+"\nPdS: " + p.getPdS() + "/" + p.getMaxPdS()+"\nPdL: " + p.getPdL() + "/" + p.getMaxPdL());
-            for(int i=0;i<p.getNbFeat();i++){
-                if(p.getFeat(i) instanceof Hammer){
-                    Hammer hammer=(Hammer)p.getFeat(i);
-                    if(hammer.getLevel()<2) {
-                        System.out.println("Hammer level "+(hammer.getLevel()+1)+": "+hammer.getGold());
-                        break;
+    public void printLog(int number) {
+        if (number == 1) {
+            for (Player p : players) {
+                System.out.println(this.players.indexOf(p) == this.getTurnPlayer() ? "\u001B[31m" + "information joueur: " + (this.players.indexOf(p) + 1) + "\u001B[0m" : "information joueur: " + (this.players.indexOf(p) + 1));
+                System.out.println("Honour: " + p.getHonour() + "\nGold: " + p.getGold() + "/" + p.getMaxGold() + "\nPdS: " + p.getPdS() + "/" + p.getMaxPdS() + "\nPdL: " + p.getPdL() + "/" + p.getMaxPdL());
+                for (int i = 0; i < p.getNbFeat(); i++) {
+                    if (p.getFeat(i) instanceof Hammer) {
+                        Hammer hammer = (Hammer) p.getFeat(i);
+                        if (hammer.getLevel() < 2) {
+                            System.out.println("Hammer level " + (hammer.getLevel() + 1) + ": " + hammer.getGold());
+                            break;
+                        }
                     }
                 }
+                p.getDice(0).toString(1);
+                p.getDice(1).toString(2);
             }
-            p.getDice(0).toString(1);
-            p.getDice(1).toString(2);
         }
     }
 
     public int[] honour() {
-        int []winner={this.players.get(0).getHonour(),0};//winner[0]=max;winner[1]=playerid;
+        int[] winner = {this.players.get(0).getHonour(), 0};//winner[0]=max;winner[1]=playerid;
         for (Player i : this.players) {
             if (winner[0] < i.getHonour()) {
                 winner[0] = i.getHonour();
@@ -119,13 +152,53 @@ public class Referee {
         return winner;
     }
 
-    public void printWinner() {
-        int []winner=this.honour();
-        System.out.println("Joueur " + (winner[1] + 1) + " gagne avec " + winner[0] + " honneurs ");
+    public void printWinner(int number) {
+        int[] winner = this.honour();
+        if (number == 1) {
+            System.out.println("Joueur " + (winner[1] + 1) + " gagne avec " + winner[0] + " honneurs ");
+        } else {
+            for (int i = 0; i < this.getNumberPlayer(); i++) {
+                if (winner[1] == i) {
+                    this.nbVictoire.set(i, this.nbVictoire.get(i) + 1);
+                }
+            }
+        }
     }
 
-    public void buy(Pool pool,int poolFace,int diceNumber,int diceFace){
-        this.getPlayer(this.turnPlayer).buy(this,pool,poolFace,diceNumber,diceFace);
+    public void buy(Pool pool, int poolFace, int diceNumber, int diceFace) {
+        this.getPlayer(this.turnPlayer).buy(this, pool, poolFace, diceNumber, diceFace);
     }
 
+    public void reset() {
+        for (Player player : players) {
+            player.reset();
+        }
+        this.round = 1;
+        Forge forge = new Forge(this);
+        this.addForge(forge);
+        World world = new World(this);
+        this.addWorld(world);
+        this.turnPlayer = 0;
+    }
+
+    public void game(int number) {
+        for (int j = 0; j < number; j++) {
+            while (this.getRound() <= this.getMaxRound()) {
+                if (number == 1) System.out.println("Nous sommes au tour : " + this.getRound() + "\n");
+                for (int i = 0; i < this.getNumberPlayer(); i++) {
+                    this.faveur();
+                    this.choixAction(this.getPlayer(this.getTurnPlayer()).chooseAction(), number);
+                    this.printLog(number);
+                    this.nextPlayer();
+                }
+            }
+            this.printWinner(number);
+            this.reset();
+        }
+        if (number > 1) {
+            for (int i = 0; i < this.getNumberPlayer(); i++) {
+                System.out.println("Le joueur " + (i+1) + ": " + this.nbVictoire.get(i));
+            }
+        }
+    }
 }
