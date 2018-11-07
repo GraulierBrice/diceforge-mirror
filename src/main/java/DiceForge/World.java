54ab095717bae6ee3b10b8a0ad4ec275caec07e8
Aldric <neogeekmo@live.fr>
2018-11-07 12:30:10 +0100

    Revert "#21 bug add passeur into world"
    This reverts commit c7015c614d261f47e01489eac0785a4b37e2d9e4.

--- src/main/java/DiceForge/World.java
+++ src/main/java/DiceForge/World.java
@@ -9,29 +9,29 @@ public class World {
     public World(Referee R){
         ArrayList<Feat> feats1 = new ArrayList<>();
         ArrayList<Feat> feats2 = new ArrayList<>();
-        //ArrayList<Feat> feats3 = new ArrayList<>();
-        //ArrayList<Feat> feats4 = new ArrayList<>();
+        ArrayList<Feat> feats3 = new ArrayList<>();
+       /*ArrayList<Feat> feats4 = new ArrayList<>();
         ArrayList<Feat> feats5 = new ArrayList<>();
         ArrayList<Feat> feats6 = new ArrayList<>();
-        //ArrayList<Feat> feats7 = new ArrayList<>();
+        ArrayList<Feat> feats7 = new ArrayList<>();*/
 
         for(int i=0;i<R.getNumberPlayer();i++){
             feats1.add(new Hammer());feats1.add(new Chest());
             feats2.add(new Ancien());feats2.add(new HerbesFolles());
-            //feats3.add();feats3.add();
-            //feats4.add(new );feats4.add();
-            feats5.add(new Passeur());//feats5.add();
-            feats6.add(new Meduse());//feats6.add();
-            //feats7.add();feats7.add();feats7.add();
+            feats3.add(new Ancien());feats3.add(new HerbesFolles());
+            /*feats4.add();feats4.add();
+            feats5.add();feats5.add();
+            feats6.add();feats6.add();
+            feats7.add();feats7.add();feats7.add();*/
 
         }
         islands.add(new Island(feats1));
         islands.add(new Island(feats2));
-        //islands.add(new Island(feats3));
-        //islands.add(new Island(feats4));
+        islands.add(new Island(feats3));
+        /*islands.add(new Island(feats4));
         islands.add(new Island(feats5));
         islands.add(new Island(feats6));
-        //islands.add(new Island(feats7));
+        islands.add(new Island(feats7));*/
     }
     /* Accessor */
     public Island getIsland(int n){return this.islands.get(n); }
