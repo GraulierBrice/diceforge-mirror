7a2dad010c82a915d7e89b0b9ca529809b8a8c1e
Elzebalth <vialestephane1@gmail.com>
2018-11-10 01:26:01 +0100

    #30 created constructor for Hydre
    there is no effect except Honour,
    added for a method which makes player at the last turn take the best thing in term of honour

---
+++ src/main/java/DiceForge/Feat/Hydre.java
@@ -0,0 +1,12 @@
+package DiceForge.Feat;
+
+public class Hydre extends Feat {
+
+    public Hydre(){
+        super(26,5,5,false);
+    }
+    @Override
+    public void effect(Object... o) {
+
+    }
+}
