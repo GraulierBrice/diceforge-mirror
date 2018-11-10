b20282994e2e02d7b03a517c1889a9c452ef1633
Elzebalth <vialestephane1@gmail.com>
2018-11-10 01:27:40 +0100

    #22 created constructor for CasqueInvisibilite
    there is no effect except Honour,
    added for a method which makes player at the last turn take the best thing in term of honour

---
+++ src/main/java/DiceForge/Feat/CasqueInvisibilite.java
@@ -0,0 +1,13 @@
+package DiceForge.Feat;
+
+public class CasqueInvisibilite extends Feat{
+
+    public CasqueInvisibilite(){
+        super(4,5,0,false);
+    }
+
+    @Override
+    public void effect(Object... o) {
+
+    }
+}
