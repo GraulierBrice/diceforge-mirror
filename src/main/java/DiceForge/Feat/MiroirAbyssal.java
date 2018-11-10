a9a9ed9d6642b7ab781fc6b4e34d756daf8f01e2
Elzebalth <vialestephane1@gmail.com>
2018-11-10 01:28:25 +0100

    #27 created constructor for MiroirAbyssal
    there is no effect except Honour,
    added for a method which makes player at the last turn take the best thing in term of honour

---
+++ src/main/java/DiceForge/Feat/MiroirAbyssal.java
@@ -0,0 +1,13 @@
+package DiceForge.Feat;
+
+public class MiroirAbyssal extends Feat{
+
+    public MiroirAbyssal(){
+        super(10,5,0,false);
+    }
+
+    @Override
+    public void effect(Object... o) {
+
+    }
+}
