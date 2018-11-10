a208eefe44bb7f94076303e3d8ca6ab041974040
Elzebalth <vialestephane1@gmail.com>
2018-11-10 01:28:06 +0100

    #20 created constructor for satyres
    there is no effect except Honour,
    added for a method which makes player at the last turn take the best thing in term of honour

---
+++ src/main/java/DiceForge/Feat/Satyres.java
@@ -0,0 +1,13 @@
+package DiceForge.Feat;
+
+public class Satyres extends Feat{
+
+    public Satyres(){
+        super(6,3,0,false);
+    }
+
+    @Override
+    public void effect(Object... o) {
+
+    }
+}
