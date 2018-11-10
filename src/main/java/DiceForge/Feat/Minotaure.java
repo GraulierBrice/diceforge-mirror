dd2484a77009634d706695b5d663c07b2624d358
Elzebalth <vialestephane1@gmail.com>
2018-11-10 01:27:00 +0100

    #25 created constructor for Minotaure
    there is no effect except Honour,
    added for a method which makes player at the last turn take the best thing in term of honour

---
+++ src/main/java/DiceForge/Feat/Minotaure.java
@@ -0,0 +1,13 @@
+package DiceForge.Feat;
+
+public class Minotaure extends Feat{
+
+    public Minotaure(){
+        super(8,0,3,false);
+    }
+
+    @Override
+    public void effect(Object... o) {
+
+    }
+}
