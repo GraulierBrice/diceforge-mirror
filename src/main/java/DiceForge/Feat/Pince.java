9679219b93d4011db7914777266649b8ee26222f
Elzebalth <vialestephane1@gmail.com>
2018-11-10 01:26:20 +0100

    #29 creating constructor for Pince
    there is no effect except Honour,
    added for a method which makes player at the last turn take the best thing in term of honour

---
+++ src/main/java/DiceForge/Feat/Pince.java
@@ -0,0 +1,13 @@
+package DiceForge.Feat;
+
+public class Pince extends Feat{
+
+    public Pince(){
+        super(8,6,0,false);
+    }
+
+    @Override
+    public void effect(Object... o) {
+
+    }
+}
