1e04f1d005c32a9404969259d65cf87eb12b074f
Elzebalth <vialestephane1@gmail.com>
2018-11-10 01:25:38 +0100

    #31 added constructor for Enigme
    there is no effect except Honour,
    added for a method which makes player at the last turn take the best thing in term of honour

---
+++ src/main/java/DiceForge/Feat/Enigme.java
@@ -0,0 +1,13 @@
+package DiceForge.Feat;
+
+public class Enigme extends Feat{
+
+    public Enigme(){
+        super(10,0,6,false);
+    }
+
+    @Override
+    public void effect(Object... o) {
+
+    }
+}
