57e328fc18382998f3fc5bd92f7ef0f23f8aad20
Elzebalth <vialestephane1@gmail.com>
2018-11-10 01:26:44 +0100

    #24 created constructor for AilesGardienne
    there is no effect except Honour,
    added for a method which makes player at the last turn take the best thing in term of honour

---
+++ src/main/java/DiceForge/Feat/AilesGardienne.java
@@ -0,0 +1,13 @@
+package DiceForge.Feat;
+
+public class AilesGardienne extends Feat{
+
+    public AilesGardienne(){
+        super(4,0,2,true);
+    }
+
+    @Override
+    public void effect(Object... o) {
+
+    }
+}
