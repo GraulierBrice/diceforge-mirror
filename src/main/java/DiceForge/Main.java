29f9cb19ffb2b3f5735f43c37e79348e4503841a
Aldric <neogeekmo@live.fr>
2018-11-18 23:03:21 +0100

    #28 Creation SolarAI
    this AI use SolarShard for buy Feat

--- src/main/java/DiceForge/Main.java
+++ src/main/java/DiceForge/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static int numberOfGames=100000;
     public static void main(String[] args) {
 
-        Referee R = new Referee(new Player("1"),new Player(new LunarAI(),"2"),new Player("3"),new Player("4"));
+        Referee R = new Referee(new Player("RandomAI_1"),new Player(new LunarAI(),"LunarAI"),new Player(new SolarAI(),"SolarAI"),new Player("RandomAI_2"));
         Forge forge = new Forge(R);
         World world = new World(R);
         Announcer announcer = new Announcer(R);
