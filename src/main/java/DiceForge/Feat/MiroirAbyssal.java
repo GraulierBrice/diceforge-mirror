9fe05fa54a864fc187dd814e87d635e084d578bb
GraulierBrice <brice.graulier@gmail.com>
2018-11-27 20:52:05 +0100

    #27 mirroir installé
    Effect in place

--- src/main/java/DiceForge/Feat/MiroirAbyssal.java
+++ src/main/java/DiceForge/Feat/MiroirAbyssal.java
@@ -4,11 +4,13 @@ public class MiroirAbyssal extends Feat{
 
     public MiroirAbyssal(){
         super(10,5,0,false);
-        this.name=nameFeat.MiroirAbyssal;
+        mirror = new FaceCombinationAND(0,0,0,0);
+        mirror.setKind("mirror");
+        this.name = nameFeat.MiroirAbyssal;
     }
 
     @Override
     public void effect(Object... o) {
-
+    	owner.getDice( (owner.getStrategy().chooseDice())%2 ).setFace(mirror,owner.getStrategy().chooseDiceFace((owner.getStrategy().chooseDice())%2));
     }
 }
