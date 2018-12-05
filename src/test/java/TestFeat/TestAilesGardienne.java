343d3f956231c4996fec7c886ac2a1871876747d
Aldric <neogeekmo@live.fr>
2018-12-05 21:05:14 +0100

    #24 correction test AilesGardienne, modification des choix des IA

--- src/test/java/TestFeat/TestAilesGardienne.java
+++ src/test/java/TestFeat/TestAilesGardienne.java
@@ -16,18 +16,112 @@ public class TestAilesGardienne {
     @Before
     public void setUp() {
         ailes = new AilesGardienne();
-        ailes.setPlayer(new Player(new LunarAI(),"LunarAI"));
+        ailes.setPlayer(new Player(new SolarAI(),"LunarAI"));
+    }
+
+    public void remove(){
         ailes.getOwner().removeGold(1000);
+        ailes.getOwner().removeSolarShard(1000);
+        ailes.getOwner().removeLunarShard(1000);
+        ailes.getOwner().removeHonour(1000);
     }
 
     @Test
     public void gotReward () {
-        assertEquals(0,ailes.getOwner().getLunarShard());
+        remove();
+
+        ailes.getOwner().addLunarShard(6);
+        ailes.getOwner().addSolarShard(4);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getLunarShard(),6);
+        assertEquals(ailes.getOwner().getSolarShard(),5);
+        remove();
+
+        ailes.getOwner().addLunarShard(4);
+        ailes.getOwner().addSolarShard(6);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getLunarShard(),5);
+        assertEquals(ailes.getOwner().getSolarShard(),6);
+        remove();
+
+        ailes.getOwner().addLunarShard(5);
+        ailes.getOwner().addSolarShard(2);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getLunarShard(),6);
+        assertEquals(ailes.getOwner().getSolarShard(),2);
+        remove();
+
+        ailes.getOwner().addLunarShard(1);
+        ailes.getOwner().addSolarShard(5);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getLunarShard(),1);
+        assertEquals(ailes.getOwner().getSolarShard(),6);
+        remove();
+
+        ailes.getOwner().addLunarShard(5);
+        ailes.getOwner().addSolarShard(4);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getLunarShard(),5);
+        assertEquals(ailes.getOwner().getSolarShard(),5);
+        remove();
+
+        ailes.getOwner().addLunarShard(4);
+        ailes.getOwner().addSolarShard(5);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getLunarShard(),5);
+        assertEquals(ailes.getOwner().getSolarShard(),5);
+        remove();
+
+
+        ailes.getOwner().addLunarShard(2);
+        ailes.getOwner().addSolarShard(2);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getLunarShard(),2);
+        assertEquals(ailes.getOwner().getSolarShard(),3);
+        remove();
+
+        ailes.getOwner().addLunarShard(0);
+        ailes.getOwner().addSolarShard(6);
         ailes.effect();
         assertEquals(ailes.getOwner().getGold(),1);
+        remove();
+        ailes.getOwner().addLunarShard(1);
+        ailes.getOwner().addSolarShard(6);
         ailes.effect();
-        assertEquals(ailes.getOwner().getGold(),2);
-        assertEquals(ailes.getOwner().getLunarShard(),0);
+        assertEquals(ailes.getOwner().getGold(),1);
+        remove();
+        ailes.getOwner().addLunarShard(2);
+        ailes.getOwner().addSolarShard(6);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getGold(),1);
+        remove();
+        ailes.getOwner().addLunarShard(3);
+        ailes.getOwner().addSolarShard(6);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getGold(),1);
+        remove();
+        ailes.getOwner().addLunarShard(5);
+        ailes.getOwner().addSolarShard(6);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getGold(),1);
+        remove();
+        ailes.getOwner().addLunarShard(6);
+        ailes.getOwner().addSolarShard(6);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getGold(),1);
+        remove();
+        ailes.getOwner().addLunarShard(5);
+        ailes.getOwner().addSolarShard(5);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getGold(),1);
+        remove();
+        ailes.getOwner().addLunarShard(6);
+        ailes.getOwner().addSolarShard(5);
+        ailes.effect();
+        assertEquals(ailes.getOwner().getGold(),1);
+        remove();
+
+
     }
 
 
